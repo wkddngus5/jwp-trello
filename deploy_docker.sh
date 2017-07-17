faeae169ad7e7ac2a6c3e354d29c9d5184783a04
brain <wkddngus5@naver.com>
2017-07-17 19:52:16 +0900

    fix_shell_script

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,3 +1,4 @@
+echo "=======docker build============"
 docker stop jwp-trello
 docker rm jwp-trello
 docker rmi jwp-trello
@@ -6,9 +7,12 @@ docker stop jwp-nginx
 docker rm jwp-nginx
 docker rmi jwp/nginx
 
+echo "========= start building ========="
 ./gradlew clean build buildDocker
-docker run -d —name jwp-trello jwp-trello
+docker run -d --name jwp-trello jwp-trello
+echo "========= server is running ========="
 
 cd nginx_docker
 docker build -t jwp/nginx .
-docker run --name jwp-trello -d -p 80:80 --link jwp-trello:jwp_trello jwp/nginx
+docker run --name jwp-nginx -d -p 80:80 --link jwp-trello:jwp_trello jwp/nginx
+echo "========= nginx is running ========="
