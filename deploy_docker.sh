d0fac7454707ffe6083de477d02e73b1114dc395
brain <wkddngus5@naver.com>
2017-07-18 19:05:26 +0900

    fix_db_schema

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,18 +1,11 @@
+docker-compose down
+
 echo "=======docker build============"
-docker stop jwp-trello
-docker rm jwp-trello
 docker rmi jwp-trello
 
-docker stop jwp-nginx
-docker rm jwp-nginx
 docker rmi jwp/nginx
 
 echo "========= start building ========="
 ./gradlew clean build buildDocker
-docker run -d --name jwp-trello jwp-trello
-echo "========= server is running ========="
 
-cd nginx_docker
-docker build -t jwp/nginx .
-docker run --name jwp-nginx -d -p 80:80 --link jwp-trello:jwp_trello jwp/nginx
-echo "========= nginx is running ========="
+docker-compose up -d
