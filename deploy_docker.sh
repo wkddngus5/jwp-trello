4591e02ad7d3477f483556a2aae21dbe654fb4fb
brain <wkddngus5@naver.com>
2017-07-20 17:10:10 +0900

    docker_login

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,11 +1,16 @@
-docker-compose down
-
 echo "=======docker build============"
+docker stop jwp-trello
+docker rm jwp-trello
 docker rmi jwp-trello
 
+docker stop jwp-nginx
+docker rm jwp-nginx
 docker rmi jwp/nginx
 
 echo "========= start building ========="
 ./gradlew clean build buildDocker
 
-docker-compose up -d
+cd nginx_docker
+docker build -t jwp/nginx .
+docker run --name jwp-nginx -d -p 80:80 --link jwp-trello:jwp_trello jwp/nginx
+echo "========= nginx is running ========="
