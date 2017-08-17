b51e2c193782f44d1194fb0eecc863b21cbf3bd9
wkddngus5 <wkddngus5@naver.com>
2017-08-17 18:52:26 +0900

    fix_shell_script

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,13 +1,12 @@
 echo "========= start application building ========="
-./gradlew clean build buildDocker
+./gradlew clean build buildDocker --stacktrace
 
 echo "======= start nginx build ============"
 cd nginx_docker
 docker build -t jwp/nginx .
 
 echo "======= stop docker ps ============"
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker-compose -p jwptrello down
 
 echo "======= start docker ps ============"
-docker-compose up -d
+docker-compose -p jwptrello up -d
